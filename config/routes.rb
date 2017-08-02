60589e7eb7b022713178d8c308742bb8bada11c5
Toshiki Iwanami <toshiki.iwa.555@gmail.com>
2017-08-02 08:36:44 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'reminders#index'
+  resources :reminders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
